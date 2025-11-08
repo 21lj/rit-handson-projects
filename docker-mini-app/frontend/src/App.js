d8c21f6023c0b2660d36664aa7dfbfb7db29452b
Lijo Joseph <lijojoseph0021@gmail.com>
2025-11-08 11:43:31 +0530

    Docker fullstack first project

---
+++ docker-mini-app/frontend/src/App.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/hello")
+      .then(res => res.json())
+      .then(data => setMessage(data.message));
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>Frontend + Backend + DB with Docker 🐳</h1>
+      <p>
+        Message from backend: <b>{message}</b>
+      </p>
+
+
+
+      <footer className="footer">
+        Lijo Joseph, RIT-MCA-SEM1
+      </footer>
+    </div>
+  );
+}
+
+export default App;
