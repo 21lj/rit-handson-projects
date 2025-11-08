22fec885b9a3f97eb126c3723f9a46e2902d3bdf
Lijo Joseph <lijojoseph0021@gmail.com>
2025-11-08 12:33:12 +0530

    use render backend url

--- docker-mini-app/frontend/src/App.js
+++ docker-mini-app/frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch("http://localhost:5000/api/hello")
+    fetch("https://two537-docker-mini-app.onrender.com/api/hello")
       .then(res => res.json())
       .then(data => setMessage(data.message));
   }, []);
